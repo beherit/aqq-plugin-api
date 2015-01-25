822d872bb45d436faadbcc28f6446214122d176c
Krzysztof Grochocki <beherit@users.noreply.github.com>
2015-01-25 19:10:56 +0100

    Added readme and license.

--- PluginAPI.h
+++ PluginAPI.h
@@ -1,12 +1,22 @@
 ﻿//---------------------------------------------------------------------------
-//Przetłumaczone SDK komunikatora AQQ z języka Delphi na bardziej ludzki C++
-//---------------------------------------------------------------------------
-//Środowisko: Embarcadero C++ Builder
-//Pełen opis: http://adn.beherit.pl
-//Autor: Krzysztof Grochocki
-//WWW: http://beherit.pl
-//E-mail: kontakt@beherit.pl
-//Jabber: im@beherit.pl
+// Copyright (C) 2008-2015 Krzysztof Grochocki
+//
+// This file is part of PluginAPI for AQQ IM
+//
+// PluginAPI for AQQ IM is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// PluginAPI for AQQ IM is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with GNU Radio; see the file COPYING. If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street,
+// Boston, MA 02110-1301, USA.
 //---------------------------------------------------------------------------
 
 #ifndef AQQ_H
